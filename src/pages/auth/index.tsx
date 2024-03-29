485c59ed3076323d348b13feda3ebd7bad6db49f
RausRD <dunecruslan1@gmail.com>
2024-03-29 15:30:39 +0200

    added main navbar pages component

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export const Auth = () => {
+	const [selected, setSelected] = useState('login')
+	return (
+		<div className="flex">
+			
+		</div>
+	)
+}
