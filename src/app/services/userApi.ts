6811406550fd7de9db4c9e88b1714bf3b9251945
RausRD <dunecruslan1@gmail.com>
2024-03-29 11:59:54 +0200

    added postsApi

--- src/app/services/userApi.ts
+++ src/app/services/userApi.ts
@@ -24,23 +24,37 @@ export const userApi = api.injectEndpoints({
       }),
     }),
     current: builder.query<User, void>({
-			query: () => ({
-				url: '/corrent',
-				method: 'GET'
-			})
-		}),
-		getUserById: builder.query<User, string>({
-			query: (id) => ({
-				url: `/user/${id}`,
-				method: 'GET'
-			})
-		}),
-		updateUser: builder.mutation<User, {userData: FormData, id: string}>({
-			query: ({userData, id}) => ({
-				url: `/users/${id}`,
-				method: 'PUT',
-				body: userData
-			})
-		})
+      query: () => ({
+        url: '/corrent',
+        method: 'GET',
+      }),
+    }),
+    getUserById: builder.query<User, string>({
+      query: id => ({
+        url: `/user/${id}`,
+        method: 'GET',
+      }),
+    }),
+    updateUser: builder.mutation<User, { userData: FormData; id: string }>({
+      query: ({ userData, id }) => ({
+        url: `/users/${id}`,
+        method: 'PUT',
+        body: userData,
+      }),
+    }),
   }),
 });
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useCurrentQuery,
+  useLazyCurrentQuery,
+  useGetUserByIdQuery,
+  useLazyGetUserByIdQuery,
+  useUpdateUserMutation,
+} = userApi;
+
+export const {
+  endpoints: { login, register, current, getUserById, updateUser },
+} = userApi;
