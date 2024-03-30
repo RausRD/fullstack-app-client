7e41bd8e26e9fd25807474b4194e8444f4155212
RausRD <dunecruslan1@gmail.com>
2024-03-30 16:23:09 +0200

    added current post

--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetPostByIdQuery } from '../../app/services/postsApi';
+import { Card } from '../../components/card';
 
 export const CurrentPost = () => {
-	return (
-		<div>CurrentPost</div>
-	)
-}
+  const params = useParams<{ id: string }>();
+  const { data } = useGetPostByIdQuery(params?.id ?? '');
+
+  if (!data) {
+    return <h2>Поста не існує</h2>;
+  }
+
+  const {
+    content,
+    id,
+    authorId,
+    comments,
+    likes,
+    author,
+    likedByUser,
+    createdAt,
+  } = data;
+
+  return (
+    <>
+      <Card
+        cardFor="current-post"
+        avatarUrl={author.avatarUrl ?? ''}
+        content={content}
+        name={author.name ?? ''}
+        likesCount={likes.length}
+        commentsCount={comments.length}
+        authorId={authorId}
+        id={id}
+        likedByUser={likedByUser}
+        createdAt={createdAt}
+      />
+    </>
+  );
+};
